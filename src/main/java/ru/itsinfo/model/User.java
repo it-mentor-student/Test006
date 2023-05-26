79d7cac04c5ceaa92ec3622ab9afa92e83626cde
Furyus81 <makebestvideo@gmail.com>
2023-05-26 13:45:07 +0300

    Some changes 2

--- src/main/java/ru/itsinfo/model/User.java
+++ src/main/java/ru/itsinfo/model/User.java
@@ -77,7 +77,9 @@ public class User {
         return password;
     }
 
-
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public String toString() {
